effa5d29b708fc67964b883cc33ca171d68f6ced
tlakffja <tlakffja3@gmail.com>
2019-07-30 10:17:04 +0900

    hello1 insert

--- GitTest/src/com/controller/TestServlet.java
+++ GitTest/src/com/controller/TestServlet.java
@@ -13,6 +13,7 @@ public class TestServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("TestServlet.doGet");
+		System.out.println("hello1");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
